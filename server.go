e4a49231991871a22baad92c25bc263f405e9b45
Jim McGaw <jpmcgaw@gmail.com>
2014-02-22 20:41:09 -0800

    code to insert people

--- server.go
+++ server.go
@@ -25,13 +25,26 @@ func main() {
 
 	http.Handle("/", router)
 
+	CreateDBTables()
+	// uncomment and run to create Jim
+	// CreatePerson()
+
+	fmt.Println("Server is running and listening at localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 type Person struct {
-	id         uint8
-	first_name string `sql:"size:50"`
-	last_name  string `sql:"size:50"`
+	Id        int64
+	FirstName string `sql:"size:50"`
+	LastName  string `sql:"size:50"`
+}
+
+func CreateDBTables() {
+	db, err := gorm.Open("mysql", "root:@/gogular")
+	if err != nil {
+		panic(err)
+	}
+	db.CreateTable(Person{})
 }
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +59,34 @@ func GetAbout(w http.ResponseWriter, r *http.Request) {
 	w.Write(someJson)
 }
 
+func CreatePerson() {
+	person := Person{
+		FirstName: "Jim",
+		LastName:  "McGaw",
+	}
+
+	db, err := gorm.Open("mysql", "root:@/gogular")
+	// TODO : load all people from MySQL db and return as JSON
+	if err != nil {
+		panic(fmt.Sprintf("Got error when connect database, the error is '%v'", err))
+	}
+
+	db.Save(&person)
+}
+
+func GetPerson() {
+	db, err := gorm.Open("mysql", "root:@/gogular")
+	// TODO : load all people from MySQL db and return as JSON
+	if err != nil {
+		panic(fmt.Sprintf("Got error when connect database, the error is '%v'", err))
+	}
+
+	d := db.DB()
+	d.Ping()
+
+	// person := db.First(&persons) // grab first record
+}
+
 func GetAllPersons(w http.ResponseWriter, r *http.Request) {
 	db, err := gorm.Open("mysql", "root:@/gogular")
 	// TODO : load all people from MySQL db and return as JSON
@@ -54,8 +95,9 @@ func GetAllPersons(w http.ResponseWriter, r *http.Request) {
 	}
 
 	d := db.DB()
-
 	d.Ping()
+
+	// db.Find(&persons)
 
 	obj := map[string]string{}
 	obj["people"] = "insert list of people here"
